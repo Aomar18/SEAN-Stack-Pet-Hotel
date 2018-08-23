8ab300619706237ca21b23174545035a783f917f
Ahmed <omar0121@umn.edu>
2018-08-23 11:11:40 -0500

    updated database with data and added underscore to name

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,2 +1,16 @@
 let PetHotelApp = angular.module('PetHotelApp' , ['ngRoute']);
 
+PetHotelApp.config(function($routeProvider){
+    $routeProvider.when('/' , {
+        templateUrl: 'views/home.html',
+        controller: 'HomeController as hc'
+    }).when('/pet' , {
+        templateUrl: 'views/pet.html' ,
+        controller: 'PetController as pc'
+    }).when('/owner' , {
+        tempateUrl: 'views/owner.html' ,
+        controller: 'OwnerController as oc'
+    }).otherwise(`<h1>Error 404: PAGE NOT FOUND</h1>
+                        <br>
+                <p> Sorry, no pets on this page! </p> `)
+})
